18920b2c7ed7dfe04fd7a6ef8726423c411d9e3b
LushiLiu57 <lushiliu1@gmail.com>
2023-09-23 17:13:03 -0400

    added auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AskQuestion = () => {
+  return <div>ASK QUESTION</div>;
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="h1-bold">h1</h1>
-      <h2 className="h2-bold">h2</h2>
-      <h3 className="h3-bold">h3</h3>
-    </main>
-  );
-}
