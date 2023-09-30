2017236df5e5fbb68dcb9b043258a1d0bfb78021
LushiLiu57 <lushiliu1@gmail.com>
2023-09-29 22:29:14 -0400

    removed unused variable

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -62,9 +62,9 @@ export async function deleteUser(params: DeleteUserParams) {
     }
 
     // Delete all Questions made by user
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     await Question.deleteMany({ author: user._id });
 
