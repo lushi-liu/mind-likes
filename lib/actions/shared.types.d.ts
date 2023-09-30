ba7f04fdb09729baaa6ad532d80b517f9dd7cb04
LushiLiu57 <lushiliu1@gmail.com>
2023-09-29 22:10:25 -0400

    Added webhooks for Clerk

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -15,3 +15,26 @@ export interface CreateQuestionParams {
   author: Schema.Types.ObjectId | IUser;
   path: string;
 }
+
+export interface CreateUserParams {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+}
+
+export interface UpdateUserParams {
+  clerkId: string;
+  updateData: {
+    name: string;
+    username: string;
+    email: string;
+    picture: string;
+  };
+  path: string;
+}
+
+export interface DeleteUserParams {
+  clerkId: string;
+}
