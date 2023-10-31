87d5bd3d1fbb79c0bef318cc51a43df0371770dc
LushiLiu57 <lushiliu1@gmail.com>
2023-10-30 22:34:06 -0400

    connected voting system to mongodb

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -43,7 +43,7 @@ const page = async ({ params, searchParams }) => {
           </Link>
           <div className="flex justify-end">
             <Votes
-              type="question"
+              type="Question"
               itemId={JSON.stringify(result._id)}
               userId={JSON.stringify(mongoUser._id)}
               upvotes={result.upvotes.length}
